a5f187fb9d3f1b6c971b1f71f0c1d574e9c7e1e2
jean <jeanvan@yahoo.com>
2022-01-20 18:14:06 +0800

    asas

---
+++ script.js
@@ -0,0 +1,3 @@
+function destroy(){
+    document.querySelector('.selection').innerHTML = " <div class = 'pricing-card'><div class = 'title'><h2>Starter Plan</h2><p>For those just getting started</p></div>"
+}
